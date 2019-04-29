bbcf1f90ae666836ec7d926bd20358c84ce7ac0c
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2019-04-29 16:07:18 -0400

    Finished fizzbuzz.c exercise

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -10,7 +10,27 @@
 */
 int fizzbuzz(int n)
 {
-
+    int counter = 0;
+    for (int i = 1; i <= 100; i++)
+    {
+        if (i % 15 == 0)
+        {
+            printf("%d FizzBuzz\n", i);
+        }
+        else if (i % 3 == 0)
+        {
+            printf("%d Fizz\n", i);
+        }
+        else if (i % 5 == 0)
+        {
+            printf("%d Buzz\n", n);
+        }
+        else
+        {
+            counter++;
+        }
+    }
+    return counter;
 }
 
 #ifndef TESTING
